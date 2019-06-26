feb29dca6463fcffbbd618b36ae114ba1e2d7666
Samuel Hawksby-Robinson <samuel@samyoul.com>
2019-06-26 16:27:53 +0100

    Added User wrapper func

--- example/RegisterBegin.php
+++ example/RegisterBegin.php
@@ -4,24 +4,31 @@ require_once "../vendor/autoload.php";
 require_once "functions.php";
 
 use SAFETECHio\FIDO2\WebAuthn;
+use Ramsey\Uuid\Uuid;
 
 try{
+    // Init WebAuthn Server
     $WebAConfig = new WebAuthn\WebAuthnConfig(
         "SAFETECHio PHP FIDO2 Example",
-        "http://localhost:8082",
-        "http://localhost:8082"          // Optional
-        //"https://example.com/images/logo.png" // Optional
+        "http://localhost",
+        "http://localhost"
+        //"https://example.com/images/logo.png"
     );
     $WebA = new WebAuthn\WebAuthnServer($WebAConfig);
-
-    $users = GetDBUsers();
 
     // Begin Registration
 
     // create or find the registering user from your data store
-    $user = $users->get($_GET["username"]);
+    $users = GetDBUsers();
 
-    // TODO wrap the DB user in class that followers the SAFETECHio\FIDO2\WebAuthn\Contracts\User interface
+    $u = $users->get($_GET["username"]);
+    $u->uuid = Uuid::uuid1()->toString();
+    $u->name = $_GET["username"];
+    $u->display_name = "";
+    $u->icon = "";
+
+    $user = WrapUser($u);
+
     list($options, $sessionData) = $WebA->BeginRegistration($user)->Make();
 
     // sessionData should be saved in the registration session
--- example/functions.php
+++ example/functions.php
@@ -2,6 +2,10 @@
 
 require_once "../vendor/autoload.php";
 
+use \SAFETECHio\FIDO2\WebAuthn\Authenticator;
+use \SAFETECHio\FIDO2\WebAuthn\Contracts;
+use \SAFETECHio\FIDO2\WebAuthn\Credential;
+
 /**
  * @return \Filebase\Database
  * @throws \Filebase\Filesystem\FilesystemException
@@ -11,6 +15,65 @@ function GetDBUsers()
     return new \Filebase\Database([
         'dir' => './db/users'
     ]);
+}
+
+function WrapUser(\Filebase\Document $user)
+{
+    return new User($user);
+}
+
+class User implements Contracts\User {
+
+    /** @var \Filebase\Document $userDoc */
+    protected $userDoc;
+
+    /** @var Credential[] $credentials */
+    protected $credentials;
+
+    public function __construct(\Filebase\Document $userDoc)
+    {
+        $this->userDoc = $userDoc;
+
+        foreach ($userDoc as $id => $credential){
+            $this->credentials[$id] = new Credential(
+                $credential->CredentialID,
+                $credential->CredentialPublicKey,
+                $credential->Format,
+                new Authenticator(
+                    $credential->authenticator->AAGUID,
+                    $credential->authenticator->Counter
+                )
+            );
+        }
+    }
+
+    public function WebAuthnID(): string
+    {
+        return $this->userDoc->uuid;
+    }
+
+    public function WebAuthnName(): string
+    {
+        return $this->userDoc->name;
+    }
+
+    public function WebAuthnDisplayName(): string
+    {
+        return $this->userDoc->display_name;
+    }
+
+    public function WebAuthnIcon(): string
+    {
+        return $this->userDoc->icon;
+    }
+
+    /**
+     * @return array
+     */
+    public function WebAuthnCredentials(): array
+    {
+        return $this->credentials;
+    }
 }
 
 /**
