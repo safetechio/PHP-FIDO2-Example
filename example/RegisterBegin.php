66473b166c4d1f0370cd4d6b1a5b04e6aca0290b
Samuel Hawksby-Robinson <samuel@samyoul.com>
2019-06-26 22:16:31 +0100

    Minor bug fixes for Begin Reg example

--- example/RegisterBegin.php
+++ example/RegisterBegin.php
@@ -10,7 +10,7 @@ try{
     // Init WebAuthn Server
     $WebAConfig = new WebAuthn\WebAuthnConfig(
         "SAFETECHio PHP FIDO2 Example",
-        "http://localhost",
+        "localhost",
         "http://localhost"
         //"https://example.com/images/logo.png"
     );
@@ -24,20 +24,25 @@ try{
     $u = $users->get($_GET["username"]);
     $u->uuid = Uuid::uuid1()->toString();
     $u->name = $_GET["username"];
-    $u->display_name = "";
+    $u->display_name = $_GET["username"];
     $u->icon = "";
+    $u->save();
 
     $user = WrapUser($u);
 
+    /**
+     * @var WebAuthn\Protocol\Options\CredentialCreation $options
+     * @var WebAuthn\SessionData $sessionData
+     */
     list($options, $sessionData) = $WebA->BeginRegistration($user)->Make();
 
     // sessionData should be saved in the registration session
     session_start();
     $_SESSION['registration_session'] = $sessionData;
 
-    WriteJSON($options);
     // respond with the options
     // options->publicKey contains the registration options
+    WriteJSON($options);
 } catch (Throwable $exception) {
     var_dump($exception);
 }
